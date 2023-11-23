12f9f8289f262ee7b8c676ddd02b5340ba99d75c
Your Name <andrewsdass2000@gmail.com>
2023-11-23 16:01:45 +0530

    search updated

--- stack.c
+++ stack.c
@@ -1,70 +1,99 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define MAX 10
+#include<limits.h>
+
+ struct values
+{
+    int data;
+    struct vaues* next;
+};
+typedef struct values node;
+node* top=NULL;
 
 
-typedef struct values
+
+
+void push(int val)
 {
-    int elements[MAX];
-    int top;
-}stack;
-void createstack(stack *ps)
-{
-    //struct stack* ps= (struct stack*) malloc(sizeof(struct stack));
-    ps->top = -1;
-    return ps;
+    node* newnode= (node*) malloc(sizeof(node));
+    newnode->data=val;
+    newnode->next=top;
+    top=newnode;
     
 }
 
-
-void push(stack *ps,int val)
-{
-    if( ps->top == (MAX-1))
-  {
-        printf("stack is full");
-    }
-    ps->elements[++ps->top] = val;
+int pop()
+{ 
+    node* del= top;
+    top=top->next;
+    int temp=del->data;
+    printf("\n pop values is :%d",temp);
+    printf("|___%d___|\n",temp);
+    free (del);
 
 }
 
-int pop(stack *ps)
+int peek()
 {
-    if(ps->top == -1)
+    if(top==NULL)
     {
-        printf("stack is empty");
-        return 0;
+        printf("list is empty");
     }
-    int temp;
-    temp= ps->elements[ps->top];
-    ps->top--;
-    return temp;
+    
+    printf("\n current value:%d",top->data);
+     printf("|___%d___|\n",top->data);
 }
 
-int peek(stack *ps)
+void display()
 {
-    if(ps->top == -1)
+    if(top==NULL) 
     {
-        printf("stack is empty");
-        return 0;
+        printf("node is empty");
     }
-    return ps->elements[ps->top];
+    node* temp=top;
+    printf("\n elements in the list are:\n");
+    while(temp!=NULL)
+    {
+        printf("%d\t\t",temp->data);
+        printf("|___%d___|\n",temp->data);
+        temp=temp->next;
+         
+    }
 }
 
-int display(stack *ps)
+int search(int target)
 {
-    int i;
-    for(i=ps->top;i>=0;i++)
+    if(top==NULL) 
     {
-       printf("%d",ps->elements[i]);
+        printf("node is empty");
+    }
+    node* temp=top;
+    
+    
+    while(temp!=NULL)
+    {
+        if(temp->data==target)
+    {
+       
+       printf("|___%d___|  <--search found\n",temp->data);
+    }
+    else
+    {
+        
+        printf("|___%d___|\n",temp->data);
+    }
+        temp=temp->next;
+         
     }
 }
+     
+
 void main()
 {
-    stack s; 
-    createstack(&s);
+    int target;
     while(1)
     {
-      int choice,val;
+      int choice,data;
       printf("\n OPTIONS");
       printf("\n 1.push ");
       printf("\n 2.pop");
@@ -81,37 +110,158 @@ void main()
       {
       case 1:
         printf("\nenter the value to push :");
-        scanf("%d",&val);
-        push(&s,val);
+        scanf("%d",&data);
+        push(data);
         break;
 
       case 2:
-        printf("\nenter the value to pop :");
-        scanf("%d",&val);
-        pop(&s);
+        pop();
         break;
 
       case 3:
-        peek(&s);
+        peek();
         break;
 
       case 4:
-        printf("\nenter the value to search :");
-        scanf("%d",&val);
-        //search(val);
+      printf("\nenter the value to search :");
+        scanf("%d",&target);
+        search(target);
         break;
 
       case 5:
-        display(&s);
+        display();
         break;  
         
         case 6:
         printf("\n exit");
+        printf("\n thank you");
+        printf("\n______________________________");
+        return 0;
+        #include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+ struct values
+{
+    int data;
+    struct vaues* next;
+};
+typedef struct values node;
+node* top=NULL;
+
+
+
+
+void push(int val)
+{
+    node* newnode= (node*) malloc(sizeof(node));
+    newnode->data=val;
+    newnode->next=top;
+    top=newnode;
+    
+}
+
+int pop()
+{ 
+    node* del= top;
+    top=top->next;
+    int temp=del->data;
+    printf("\n pop values is :%d",temp);
+    printf("|___%d___|\n",temp);
+    free (del);
+
+}
+
+int peek()
+{
+    if(top==NULL)
+    {
+        printf("list is empty");
+    }
+    
+    printf("\n current value:%d",top->data);
+     printf("|___%d___|\n",top->data);
+}
+
+void display()
+{
+    if(top==NULL) 
+    {
+        printf("node is empty");
+    }
+    node* temp=top;
+    printf("\n elements in the list are:");
+    while(temp!=NULL)
+    {
+        printf("%d\t\t",temp->data);
+        printf("|___%d___|\n",temp->data);
+        temp=temp->next;
+         
+    }
+}
+     
+
+void main()
+{
+    
+    while(1)
+    {
+      int choice,data;
+      printf("\n OPTIONS");
+      printf("\n 1.push ");
+      printf("\n 2.pop");
+      printf("\n 3.peek");
+      //printf("\n 4.search");
+      printf("\n 5.display");
+      printf("\n 6.exit");
+      printf("\n.......................................");
+      printf("\n enter the choice:");
+      scanf("%d",&choice);
+      printf("\n.......................................");
+
+      switch (choice)
+      {
+      case 1:
+        printf("\nenter the value to push :");
+        scanf("%d",&data);
+        push(data);
         break;
 
-      
-      default:
+      case 2:
+        pop();
         break;
+
+      case 3:
+        peek();
+        break;
+
+      case 4:
+      printf("\nenter the value to search :");
+        scanf("%d",&data);
+        //search(val);
+        break;
+
+      case 5:
+        display();
+        break;  
+        
+        case 6:
+        printf("\n exit");
+        printf("\n thank you");
+        printf("\n______________________________");
+        return 0;
+        
+
+      
+      default:printf("invalid code:");
+
+      }
+    }
+}
+
+      
+      default:printf("invalid code:");
+
       }
     }
 }
